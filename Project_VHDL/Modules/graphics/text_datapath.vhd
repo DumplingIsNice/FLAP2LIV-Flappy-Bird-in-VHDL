ae0558a74812371a06bef2c9bc40435c49999c19
uoa-hlin784 <hlin784@aucklanduni.ac.nz>
2021-05-08 16:33:31 +1200

    added VHDL for text_datapath (and testbench) and text_package
    Update: Most recent git-push from me is a component (and relevant package) meant to feed the correct information to char_rom based on the position of the pixel currently refreshed on screen (without scaling).
    
    This component currently SHOULD work together with char_rom and VGA_sync. However, ideally, it should be a part of GUI and eventually Graphics.
    
    I am 40% sure it will work in its current state. Not yet synthesized. Further testing required in Quartus and on DE0 board.

---
+++ Project_VHDL/Modules/graphics/text_datapath.vhd
@@ -0,0 +1,102 @@
+-- This component is the datapath for feeding the correct information to char-rom
+-- To fetch the appropriate rom_mux_output for the current pixel position on the window.
+
+    -- Input: The current pixel position on the display window from VGA_sync.
+    -- Outputs: All necessary information for char_rom to output a rom_mux_output
+    -- (font_row, font_col, address), the scale output is for debugging purposes only.
+
+-- Position of each character to be displayed on screen is currently defined 
+-- as a FONT_CHAR record in text_pkg.vhd
+
+-- NOTE: x -> col, y -> row
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE IEEE.STD_LOGIC_ARITH.all;
+USE IEEE.STD_LOGIC_UNSIGNED.all;
+
+USE work.text_pkg.all;
+
+ENTITY text_datapath IS
+	PORT
+	(
+		-- character_address	:	IN STD_LOGIC_VECTOR (5 DOWNTO 0);
+		row, col	        :	IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+		clock				: 	IN STD_LOGIC;
+		font_row, font_col  :   OUT STD_LOGIC_VECTOR(9 DOWNTO 0);
+        scale               :   OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+        o_address           :   OUT STD_LOGIC_VECTOR (5 DOWNTO 0)
+	);
+END text_datapath;
+
+ARCHITECTURE path OF text_datapath IS
+
+    -- Intermediate output signals.
+    SIGNAL	i_font_row, i_font_col  : STD_LOGIC_VECTOR(9 DOWNTO 0);
+    SIGNAL  i_scale                 : STD_LOGIC_VECTOR(2 DOWNTO 0);
+    SIGNAL  i_o_address             : STD_LOGIC_VECTOR (5 DOWNTO 0);
+
+    -- Procedure for checking whether the current window pixel psition lies in of of our
+    -- defined x, y position of a character.
+    -- If yes, began cell_check. Else skip.
+    PROCEDURE LIMIT_CHECK
+        (   
+            SIGNAL row, col                   : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+            SIGNAL char                       : IN FONT_CHAR;
+            SIGNAL font_row, font_col         : OUT STD_LOGIC_VECTOR(9 DOWNTO 0);
+            SIGNAL scale                      : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+            SIGNAL o_address                  : OUT STD_LOGIC_VECTOR (5 DOWNTO 0)
+        ) IS
+
+        variable col_relative : STD_LOGIC_VECTOR(9 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(0, 10); 
+        variable row_relative : STD_LOGIC_VECTOR(9 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(0, 10); 
+
+    BEGIN 
+
+        IF (((char.x <= col) and (col <= (char.x + 7)*char.scale)) 
+            and 
+            ((char.y <= row) and (row <= (char.y + 7)*char.scale))) THEN
+
+            col_relative := col - char.x;
+            row_relative := row - char.y;
+
+            font_col <= col_relative;
+            font_row <= row_relative;
+            scale <= char.scale;
+            o_address <= char.address;
+        ELSE
+            font_col <= CONV_STD_LOGIC_VECTOR(0, i_font_col'LENGTH);
+            font_row <= CONV_STD_LOGIC_VECTOR(0, i_font_row'LENGTH);
+            scale <= CONV_STD_LOGIC_VECTOR(0, i_scale'LENGTH);
+            o_address <= CONV_STD_LOGIC_VECTOR(0, i_o_address'LENGTH);
+        END IF;
+
+    END PROCEDURE LIMIT_CHECK;
+
+    SIGNAL a_1 : FONT_CHAR;
+
+BEGIN
+
+    a_1 <= CHAR_A;
+
+    PROCESS (clock)
+    BEGIN
+        IF (rising_edge(clock)) THEN
+            -- Output varification, dubugging
+            -- i_font_col <= a_1.x;
+            -- i_font_row <= a_1.y;
+            -- i_scale <= a_1.scale;
+            -- i_o_address <= a_1.address;
+            LIMIT_CHECK(
+                        row, col, a_1, 
+                        i_font_row, i_font_col, i_scale, i_o_address
+                        );
+        END IF;
+    end PROCESS;
+
+    font_row <= i_font_row;
+    font_col <= i_font_col;
+    scale <= i_scale;
+    o_address <= i_o_address;
+
+END path;
---
+++ Project_VHDL/Testbenches/TextDatapathText/test_text_datapath.vhd
@@ -0,0 +1,81 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+USE IEEE.STD_LOGIC_ARITH.all;
+
+entity test_text_datapath is
+end entity test_text_datapath;
+
+architecture my_test of test_text_datapath is
+    signal  t_row, t_col	        : STD_LOGIC_VECTOR(9 DOWNTO 0);
+	signal  t_clock				    : STD_LOGIC;
+	signal	t_font_row, t_font_col  : STD_LOGIC_VECTOR(9 DOWNTO 0);
+    signal  t_scale                 : STD_LOGIC_VECTOR(2 DOWNTO 0);
+    signal  t_o_address             : STD_LOGIC_VECTOR (5 DOWNTO 0);
+    
+    component text_datapath IS
+	PORT(   row, col	        : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+            clock				: IN STD_LOGIC;
+            font_row, font_col  : OUT STD_LOGIC_VECTOR(9 DOWNTO 0);
+            scale               : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+            o_address           : OUT STD_LOGIC_VECTOR (5 DOWNTO 0)
+        );
+    end component text_datapath;
+    
+begin
+    DUT: text_datapath port map (row => t_row, 
+                                clock => t_clock,
+                                col => t_col, 
+                                font_col => t_font_col,
+                                font_row => t_font_row,
+                                scale => t_scale,
+                                o_address => t_o_address);
+    -- setialization process (code that executes only once).
+
+-- clock generation
+    clock_gen: process
+    begin
+        t_clock <= '0';
+        wait for 5 ns;
+        t_clock <= '1';
+        wait for 5 ns;
+    end process clock_gen;
+
+-- row, col generation
+-- The font fow and column refers to the row and column of the pixel position 
+-- returned from the window from VGC_sync
+-- Check character's position definition in text_pkg.vhd
+    row_col_gen: process
+    begin
+        t_row <= conv_std_logic_vector(0, t_row'length);
+        t_col <= conv_std_logic_vector(0, t_col'length);
+        wait for 50 ns;
+        t_row <= conv_std_logic_vector(50, t_row'length);
+        t_col <= conv_std_logic_vector(100, t_col'length);
+        wait for 50 ns;
+
+        -- Test case for col/x position
+        for i in 1 to 8 loop
+            t_col <= conv_std_logic_vector((unsigned(t_col) + 1), t_col'length);
+            wait for 10 ns;
+        end loop;
+
+        wait for 50 ns;
+        t_row <= conv_std_logic_vector(50, t_row'length);
+        t_col <= conv_std_logic_vector(100, t_col'length);
+        wait for 50 ns;
+
+        -- Test case for row/y position
+        for i in 1 to 8 loop
+            t_row <= conv_std_logic_vector((unsigned(t_row) + 1), t_row'length);
+            wait for 10 ns;
+        end loop;
+
+        -- Test case for col/x and row/y position
+        wait for 50 ns;
+        t_row <= conv_std_logic_vector(58, t_row'length);
+        t_col <= conv_std_logic_vector(108, t_col'length);
+        wait for 50 ns;
+    end process row_col_gen;
+    
+end architecture my_test;
